c3fbb3616d1caf0d6f3cd5ec61503a458f745ee7
aarjav89 <53356396+aarjav89@users.noreply.github.com>
2020-01-28 20:30:54 -0500

    My Todo list

---
+++ todo-list/src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+    <h1> Aarjav's To-Do List </h1>
+    <input type="text" v-model=task>
+     <button @click="add()">Add</button>
+        <!-- whatever u type in text box appears in the console-> vue section -->
+        <h1> There are {{tasks.length}} tasks in your list</h1>
+
+         <!-- code to display array items -->
+        <ul>
+            <li v-for="(t,index) in tasks" :key="index" @click="remove(index)"> <!-- assume we are using for-each loop wherein t is the actual content -->
+                {{t}}
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+            task:"",
+                tasks:[]
+            }
+        }, // end data
+
+        methods:{
+            add() {
+                this.tasks.push(this.task)
+                this.task=""
+            },
+
+            remove(index)
+            {
+                this.tasks.splice(index,1)
+            }
+        }
+
+    }
+</script>
